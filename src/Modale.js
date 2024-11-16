21335673d7d902e9dae58e5e8994251d304face5
Ofeeling <timothee.meunier@gmail.com>
2024-11-16 23:55:19 +0100

    Add _redirects for proper routing on Netlify

--- src/Modale.js
+++ src/Modale.js
@@ -1,7 +1,7 @@
 import "./Modale.css";
 
 const Modale = ({ character, setIsOpen }) => {
-  const placeholderImage = "https://img.freepik.com/photos-premium/chute-neige-fond-noir_34266-792.jpg?w=826"; // Replace with the actual URL of your placeholder image
+  const placeholderImage = "https://img.freepik.com/photos-premium/chute-neige-fond-noir_34266-792.jpg?w=826"; 
 
   return (
     <div
@@ -10,7 +10,7 @@ const Modale = ({ character, setIsOpen }) => {
     >
       <div className="modal-content" onClick={(e) => e.stopPropagation()}> {/* Prevents closing when clicking inside the modal */}
         <img
-          src={character.image || placeholderImage} // If character.image is not available, use the placeholder image
+          src={character.image || placeholderImage} 
           alt="character from Harry Potter"
         />
         <div>Nom : {character.name}</div>
